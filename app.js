5730eac17d5133dc567c01ea763d6899b6004d93
csandeepkumardev01 <64784427+csandeepkumardev01@users.noreply.github.com>
2020-08-27 21:07:43 +0530

    removed path

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
+//app.use('/public', express.static(path.join(__dirname, 'public')))
 
 //Swagger Confguration
 const swaggerDoc = require('./swaggerDoc');
