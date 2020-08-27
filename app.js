c8c0f0ebdad67165eca53cdecdab46a7c4f0089b
csandeepkumardev01 <64784427+csandeepkumardev01@users.noreply.github.com>
2020-08-27 22:14:33 +0530

    changed port

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use("/api", require("./routes/index"));
 //     res.sendFile(path.join(__dirname, 'index.html'));
 // });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
 
